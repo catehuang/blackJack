d681634c6ca4bbe8f3cedb4a8ab8a15f7f2b8e73
catehuang <s0350228@gmail.com>
2022-08-27 22:18:23 -0600

    add replay flow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
-from replit import clear
 
-def inital_cards():
+total_cards = []
+
+def initial_cards():
     """ Prepares all cards"""
     symbols = ["red diamonds", "black clubs", "red hearts", "black spades"]
     for s in symbols:
@@ -44,56 +45,63 @@ def calculate_score(cards):
         number_of_ace -= 1
     return total
 
-total_cards = []
-user_cards = []
-computer_cards = []
-inital_cards()
-is_game_over = False
+def play_game():
+    user_cards = []
+    computer_cards = []
+    initial_cards()
+    is_game_over = False
 
-for _ in range (1, 3):
-    user_cards.append(deal_card())
+    for _ in range (1, 3):
+        user_cards.append(deal_card())
 
-for _ in range (1, 3):
-    computer_cards.append(deal_card())
+    for _ in range (1, 3):
+        computer_cards.append(deal_card())
 
-while not is_game_over:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
 
-    print(f"Your cards: {user_cards}, current score: {user_score}")
-    # print(f"Computer cards: {computer_cards}, current score: {computer_score}")
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        # print(f"Computer cards: {computer_cards}, current score: {computer_score}")
 
-    if user_score == 21 or computer_score == 21 or user_score > 21:
-        is_game_over = True
-        if user_score == 21:
-            print("You win! Black Jack")
-        elif computer_score == 21:
-            print("You lose! Bust")
-        else:
-            print("You lose!")
-    else:
-        user_decision = input("Type 'y' to get another card, type 'n' to pass: ")
-        if user_decision == 'y':
-            user_cards.append(deal_card())
-        else:
+        if user_score == 21 or computer_score == 21 or user_score > 21:
             is_game_over = True
+            if user_score == 21:
+                print("\nYou win! Black Jack")
+            elif computer_score == 21:
+                print("\nYou lose! Bust")
+            else:
+                print("\nYou lose!")
+        else:
+            user_decision = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_decision == 'y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+                while computer_score < user_score and computer_score < 16:
+                    computer_cards.append(deal_card())
+                    computer_score = calculate_score(computer_cards)
 
-
-while computer_score < user_score and computer_score < 16:
-    computer_cards.append(deal_card())
-    computer_score = calculate_score(computer_cards)
-
-print()
-if user_score < 21:
-    if computer_score > 21:
-        print("You win")
-    else:
-        if user_score == computer_score:
-            print("Draw")
-        elif user_score > computer_score:
+    print()
+    if user_score < 21:
+        if computer_score > 21:
             print("You win!")
         else:
-            print("You lose")
+            if user_score == computer_score:
+                print("Draw.")
+            elif user_score > computer_score:
+                print("You win!")
+            else:
+                print("You lose!")
 
-print(f"Your cards: {user_cards}, final score: {user_score}")
-print(f"Computer cards: {computer_cards}, final score: {computer_score}")
+    print("-" * 100)
+    print(f"Your cards: {user_cards}, final score: {user_score}")
+    print(f"Computer cards: {computer_cards}, final score: {computer_score}")
+    print("-" * 100 + "\n")
+
+    play_again = input("Do you want to play again? 'y' to play again, 'q' to quit: ")
+    if play_again == 'y':
+        print()
+        play_game()
+
+play_game()
